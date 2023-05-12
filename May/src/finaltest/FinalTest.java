f28a1faaa994c5f768e9934329a2830f9159eff3
tualatin <tualatin@163.com>
2023-05-12 17:50:35 +0800

    commit FinalTest

---
+++ May/src/finaltest/FinalTest.java
@@ -0,0 +1,11 @@
+package finaltest;
+
+
+public class FinalTest {
+    public static void main(String[] args) {
+        int a=1;
+        System.out.println(a);
+        //final a=2;
+        System.out.println(a);
+    }
+}
