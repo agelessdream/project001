195ba01e8adde076c1ef520f56d9293bdc170b51
revolution <revolution@163.com>
2023-07-01 17:01:11 +0800

    202307011701

--- JavaWeb2/src/servlet/UserServlet.java
+++ JavaWeb2/src/servlet/UserServlet.java
@@ -11,15 +11,8 @@ import java.io.IOException;
 public class UserServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        String username=req.getParameter("username");
-//
-//        if("atguigu".equals(username)){
-//            //通过响应对象响应信息
-//            resp.getWriter().write("NO");
-//        }else{
-//            resp.getWriter().write("YES");
-//        }
-        resp.getWriter().write("哈哈哈");
-          System.out.println("嘿嘿嘿");
+        resp.setContentType("text/html;charset=gbk");
+        resp.getWriter().write("尚硅谷");
+        System.out.println("嘿嘿嘿");
     }
 }
